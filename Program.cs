028dcbc4d18282a945628861cc5c7809b1a6c494
Facu <Facudiz13@gmail.com>
2023-04-27 15:59:40 -0300

    Ejercicio 20

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Billetes;
+namespace Ejercicio20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Euro e = 100;
+            Dolar d = 500;
+
+            Euro auxEuro = e + d;
+            Console.WriteLine(auxEuro.getCantidad());
+        }
+    }
+}
